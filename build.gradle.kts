335d087dd260f594a0800941b451ad1666121b1c
Christoph Knauf <knauf.christoph@gmail.com>
2022-05-12 21:21:46 +0200

    maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,14 @@ plugins {
     kotlin("jvm") version "1.6.10"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     jacoco
+
+    `java-library`
+    `maven-publish`
+    signing
 }
 
-group = "com.github.chriskn"
-version = "0.0.1-SNAPSHOT"
+group = "io.github.chriskn"
+version = "0.1.0"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -53,14 +57,71 @@ detekt {
 
 
 java {
+    withJavadocJar()
     withSourcesJar()
 }
 
-//tasks.jar {
-//    val dependencies = configurations
-//        .api
-//        .get()
-//     val mapped = dependencies.map(::zipTree)
-//    from(mapped)
-//    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-//}
+
+publishing {
+    val uri = "github.com/chriskn/structurizr-c4puml-extension"
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "structurizr-c4puml-extension"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Structurizr C4Puml Extension")
+                description.set(
+                    "Kotlin based extension to the Structurizr java library in order to use extended C4Puml features"
+                )
+                url.set("https//:$uri")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("chriskn")
+                        name.set("Christoph Knauf")
+                        email.set("knauf.christoph@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:https//:$uri.git")
+                    developerConnection.set("scm:git://$uri.git")
+                    url.set("https//:$uri")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = project.properties["ossrhUsername"].toString()
+                password = project.properties["ossrhPassword"].toString()
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
