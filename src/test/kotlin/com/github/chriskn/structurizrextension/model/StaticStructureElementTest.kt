08591e919dfe8b16c00767e34f00c22bdde80b64
chris kn <knauf.christoph@gmail.com>
2024-05-28 15:35:58 +0200

    extend StaticStructureElementTest

--- src/test/kotlin/com/github/chriskn/structurizrextension/model/StaticStructureElementTest.kt
+++ src/test/kotlin/com/github/chriskn/structurizrextension/model/StaticStructureElementTest.kt
@@ -1,18 +1,49 @@
 package com.github.chriskn.structurizrextension.model
 
 import com.structurizr.Workspace
+import com.structurizr.model.Container
+import com.structurizr.model.Model
+import com.structurizr.model.Person
+import com.structurizr.model.SoftwareSystem
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class StaticStructureElementTest {
 
-    @Test
-    fun `throws IllegalArgumentException if element uses person`() {
-        val model = Workspace("", "").model
-        val person = model.person("test", "")
+    private lateinit var model: Model
+    private lateinit var person: Person
+    private lateinit var system: SoftwareSystem
+    private lateinit var container: Container
 
+    @BeforeAll
+    fun setUp(){
+        model = Workspace("", "").model
+        person = model.person("test", "")
+        system =  model.softwareSystem("test system", "")
+        container = system.container("test container", "")
+    }
+
+    @Test
+    fun `throws IllegalArgumentException if SoftwareSystem uses person`() {
         assertThrows<IllegalArgumentException> {
-            model.softwareSystem("test system", "", uses = listOf(Dependency(person, "invalid")))
+            model.softwareSystem("test system", "")
+            system.uses(person, "invalid")
+        }
+    }
+
+
+    @Test
+    fun `throws IllegalArgumentException if Container uses person`() {
+        assertThrows<IllegalArgumentException> {
+            container.uses(person, "invalid")
+        }
+    }
+
+    @Test
+    fun `throws IllegalArgumentException if Component uses person`() {
+            assertThrows<IllegalArgumentException> {
+            container.component("component", "", uses = listOf(Dependency(person, "invalid")))
         }
     }
 }
